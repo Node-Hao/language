5f9c12254dee29517e6c23a6f174f78e5a105c01
Node-Hao <yunc58451@gmail.com>
2025-09-25 23:10:44 +0800

    hashtable

--- algorithm_test.cpp
+++ algorithm_test.cpp
@@ -74,92 +74,212 @@ int main() {
  */
 
 //哨兵节点无需考虑头结点的情况
-struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
-class Solution {
+// struct ListNode {
+//       int val;
+//       ListNode *next;
+//       ListNode() : val(0), next(nullptr) {}
+//       ListNode(int x) : val(x), next(nullptr) {}
+//       ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  };
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         //处理极端情况
+//         if(!head) return head;
+//         ListNode* dummy = new ListNode(0, head);
+//         ListNode* prev = dummy;
+//         ListNode* cur = head;
+
+//         while(cur)
+//         {
+//             if(cur->val == val)
+//             {
+//                 //说明此时需要删除
+//                 prev->next = cur->next;
+//                 delete cur;
+//                 cur = prev->next;
+//             }
+//             else
+//             {
+//                 prev = prev->next;
+//                 cur = cur->next;
+//             }
+//         }
+//         //哨兵节点的真正作用在于记录头结点，从而无需单独考虑删除头结点的情况
+//         ListNode* result = dummy->next;
+//         delete dummy;
+
+//         return result;
+//     }
+// };
+// //无哨兵节点需要考虑头结点
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+
+//         //删除头结点
+//         while(head != nullptr && head->val == val)
+//         {
+//             ListNode* temp = head;
+//             head = head->next;
+//             delete temp;
+//         }
+//         ListNode* cur = head;
+//         //非头结点
+//         while(cur != nullptr && cur->next != nullptr)
+//         {
+//             if(cur->next->val == val)
+//             {
+//                 ListNode* temp = cur->next;
+//                 cur->next = cur->next->next;
+//                 delete temp;
+//             }
+//             else
+//             {
+//                 cur = cur->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
+// //递归删除的策略
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         if(!head) return nullptr;
+
+//         if(head->val == val)
+//         {
+//             ListNode* newNode =  removeElements(head->next, val);
+//             delete head;
+//             return newNode;
+//         }
+//         else
+//         {
+//             head->next = removeElements(head->next, val);
+//             return head;
+//         }
+//     }
+// };
+
+
+#include <iostream>
+#include <deque>
+#include <vector>
+// //把 deque 当做是滑动窗口，往窗口中塞入元素的同时也要比较大小，当前元素与窗口末尾的进行比较
+
+// void slidingWindowMaximum(const std::vector<int>& nums, int k) {
+//     std::deque<int> window;
+    
+//     for (size_t i = 0; i < nums.size(); ++i) {
+//         // 移除窗口左边界之外的元素
+//         if (!window.empty() && window.front() == i - k) {
+//             window.pop_front();
+//         }
+        
+//         // 保持 `deque` 的递减顺序
+//         while (!window.empty() && nums[i] > nums[window.back()]) {
+//             window.pop_back();
+//         }
+        
+//         // 添加当前元素的索引
+//         window.push_back(i);
+        
+//         // 从窗口开始时输出最大值
+//         if (i >= k - 1) {
+//             std::cout << nums[window.front()] << " ";
+//         }
+//     }
+//     std::cout << std::endl;
+// }
+class MyLinkedList {
 public:
-    ListNode* removeElements(ListNode* head, int val) {
-        //处理极端情况
-        if(!head) return head;
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* prev = dummy;
-        ListNode* cur = head;
-
-        while(cur)
-        {
-            if(cur->val == val)
-            {
-                //说明此时需要删除
-                prev->next = cur->next;
-                delete cur;
-                cur = prev->next;
-            }
-            else
-            {
-                prev = prev->next;
-                cur = cur->next;
-            }
-        }
-        //哨兵节点的真正作用在于记录头结点，从而无需单独考虑删除头结点的情况
-        ListNode* result = dummy->next;
-        delete dummy;
-
-        return result;
+    struct LinkedNode{
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val): val(val), next(nullptr){}
+    };
+    //初始化链表
+    MyLinkedList() {
+        //需要一个哨兵节点
+        dummy = new LinkedNode(0);
+        size = 0;
     }
-};
-//无哨兵节点需要考虑头结点
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-
-        //删除头结点
-        while(head != nullptr && head->val == val)
+    
+    int get(int index) {
+        if(index < 0 || index >= size)
         {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
+            return -1;
         }
-        ListNode* cur = head;
-        //非头结点
-        while(cur != nullptr && cur->next != nullptr)
+        //拿到头结点
+        LinkedNode* cur = dummy->next;
+        while(index--)
         {
-            if(cur->next->val == val)
-            {
-                ListNode* temp = cur->next;
-                cur->next = cur->next->next;
-                delete temp;
-            }
-            else
-            {
-                cur = cur->next;
-            }
+            cur = cur->next;
         }
-        return head;
+        return cur->val;
     }
-};
-//递归删除的策略
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        if(!head) return nullptr;
-
-        if(head->val == val)
-        {
-            ListNode* newNode =  removeElements(head->next, val);
-            delete head;
-            return newNode;
-        }
-        else
-        {
-            head->next = removeElements(head->next, val);
-            return head;
-        }
+    
+    void addAtHead(int val) {
+        //头插法
+        LinkedNode* newNode = new LinkedNode(val);
+        newNode->next = dummy->next;
+        dummy->next = newNode;
+        ++size;
     }
+    
+    void addAtTail(int val) {
+        //尾插法
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = dummy;//有可能为空链表
+        while(cur->next)
+        {
+            cur = cur->next;
+        }
+        cur->next = newNode;
+        ++size;        
+    }
+    
+    void addAtIndex(int index, int val) {
+        if(index > size) return;
+        if(index < 0) index = 0;
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = dummy;
+        while(index--)
+        {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+        ++size;
+    }
+    
+    void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) return;
+        //任意位置删除
+        LinkedNode* cur = dummy;
+        while(index--)
+        {
+            cur = cur->next;
+        }
+        //此时 cur 走到了待删除节点的前一个位置，需要记录待删除节点的下一个位置
+        LinkedNode* tmp = cur->next;
+        cur->next = tmp->next;
+        delete tmp;
+        tmp = nullptr;
+        --size;
+    }
+private:
+    LinkedNode* dummy;
+    size_t size; 
 };
 
-
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
---
+++ container/hashtable.cpp
@@ -0,0 +1,147 @@
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <utility>
+#include <vector>
+#include <sstream>
+#include <string>
+
+template <typename Key, typename Value, typename Hash = std::hash<Key>>
+class HashTable
+{
+    class HashNode
+    {
+    public:
+        Key key;
+        Value value;
+        // 从 key 构造节点
+        explicit HashNode(const Key &key) : key(key), value() {};
+        // 从 key 和 value 构造节点
+        HashNode(const Key &key, const Value &value) : key(key), value(value) {}
+        // 比较重载运算符
+        bool operator==(const HashNode &other) const { return key == other.key; }
+        bool operator!=(const HashNode &other) const { return key != other.key; }
+        bool operator<(const HashNode &other) const { return key < other.key; }  // 修复缺少的闭合括号
+        bool operator>(const HashNode &other) const { return key > other.key; }  // 修复比较逻辑错误
+        bool operator==(const Key &key_) const { return key == key_; }
+        
+        void print() const
+        {
+            std::cout << key << " " << value << " ";
+        }
+    };
+private:
+    using Bucket = std::list<HashNode>;  // 定义桶的类型为存储键的链表
+    std::vector<Bucket> buckets;  // 定义所有桶的动态数组
+    Hash hashFunction;  // 哈希函数对象
+    size_t tableSize;  // 哈希表的大小
+    size_t numElements;  // 哈希表中元素的数量
+
+    float maxLoadFactor = 0.75;  // 默认的最大负载因子
+
+    // 计算键的哈希值，并将其映射到桶的索引
+    size_t hash(const Key &key) const { return hashFunction(key) % tableSize; }
+    
+    // 当元素数量超过最大负载因子定义的容量时，增加桶的数量并重新分配所有键
+    void rehash(size_t newSize)
+    {
+        if (newSize == 0) return;  // 避免无效的新大小
+        std::vector<Bucket> newBuckets(newSize);  // 创建新的桶数组
+
+        for (Bucket& bucket : buckets)  // 遍历旧桶数组
+        {
+            for (HashNode& hashNode : bucket)  // 遍历桶中的节点
+            {
+                size_t newIndex = hashFunction(hashNode.key) % newSize;
+                newBuckets[newIndex].push_back(hashNode);
+            }
+        }
+        buckets = std::move(newBuckets);  // 使用移动语义更新桶数组
+        tableSize = newSize;  // 更新哈希表大小
+    }
+
+public:
+    // 构造函数初始化哈希表
+    HashTable(size_t size = 10, const Hash& hashFunc = Hash())
+        : buckets(size), hashFunction(hashFunc), tableSize(size), numElements(0) {}
+
+    // 插入键值对到哈希表中
+    void insert(const Key& key, const Value& value)
+    {
+        // 检查是否需要重哈希???为什么要 + 1
+        if ((numElements + 1) > maxLoadFactor * tableSize)
+        {
+            // 处理 clear 后再次插入元素时 tableSize = 0 的情况
+            if (tableSize == 0) tableSize = 1;
+            rehash(tableSize * 2);  // 重哈希, 桶数量翻倍
+        }
+        size_t index = hash(key);  // 计算桶的索引
+        std::list<HashNode>& bucket = buckets[index];  // 获取对应的桶
+        // 如果键不在桶中，则添加到桶中
+        if (std::find(bucket.begin(), bucket.end(), key) == bucket.end())
+        {
+            bucket.push_back(HashNode(key, value));
+            ++numElements;  // 增加元素数量（修复注释错误）
+        }
+    }
+    
+    void insertKey(const Key& key) { insert(key, Value{}); }
+
+    // 从哈希表中删除键
+    void erase(const Key& key)  // 修复返回值错误
+    {
+        if (tableSize == 0) return;  // 空表直接返回
+        size_t index = hash(key);  // 计算键的索引
+        auto& bucket = buckets[index];  // 获取对应的桶
+        // 查找键是否在桶中
+        auto it = std::find(bucket.begin(), bucket.end(), key);
+        if (it != bucket.end())
+        {
+            bucket.erase(it);  // 执行删除操作
+            --numElements;     // 更新元素数量
+        }
+    }
+
+    // 查找键是否存在于哈希表中
+    Value* find(const Key &key)
+    {
+        if (tableSize == 0) return nullptr;  // 空表直接返回
+        size_t index = hash(key);
+        auto& bucket = buckets[index];
+        // 查找键是否在桶中
+        auto it = std::find(bucket.begin(), bucket.end(), key);
+        if (it != bucket.end())
+        {
+            return &it->value;
+        }
+        return nullptr;
+    }
+    
+    // 获取哈希表中元素的数量
+    size_t size() const { return numElements; }
+
+    // 打印哈希表中的所有元素
+    void print() const
+    {
+        for (size_t i = 0; i < buckets.size(); ++i)
+        {
+            if (!buckets[i].empty())  // 只打印非空桶
+            {
+                for (const HashNode& node : buckets[i])
+                {
+                    node.print();
+                }
+            }
+        }
+        std::cout << std::endl;
+    }
+    
+    void clear()
+    {
+        buckets.clear();  // 修复成员名拼写错误
+        numElements = 0;
+        tableSize = 0;
+    }
+};
