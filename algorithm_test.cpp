da7fa1ba468576a7d8f4d77e18545a06e3ab5095
Node-Hao <yunc58451@gmail.com>
2025-10-10 16:11:22 +0800

    10_10

--- algorithm_test.cpp
+++ algorithm_test.cpp
@@ -713,3 +713,134 @@ struct ListNode {
 //         return result;
 //     }
 // };
+
+// 翻转字符串II
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stdexcept> // 用于异常处理
+#include <sstream>
+#include <string>
+
+template <typename T, typename Cotainer = std::deque<T>>
+class MyStack
+{
+    private:
+        Cotainer data;// 使用底层容器存储栈的元素
+
+    public:
+        // 压入元素到栈顶
+        void push(const T& value)
+        {
+            data.push_back(value);
+        }
+        // 弹出栈顶元素
+        void pop()
+        {
+            if (!empty())
+            {
+                data.pop_back();
+            }
+            else
+            {
+                throw std::runtime_error("Stack is empty.");
+            }
+        }
+        // 返回栈顶元素的引用
+        T& top()
+        {
+            if (!empty())
+            {
+                return data.back();
+            }
+            else
+            {
+                throw std::runtime_error("Stack is empty.");
+            }
+        }
+
+        // 检查栈是否为空
+        bool empty() const
+        {
+            return data.empty();
+        }
+
+        // 返回栈的大小
+        size_t size() const
+        {
+            return data.size();
+        }
+};
+
+int main() {
+        // 使用 std::deque 作为底层容器的示例
+    MyStack<int, std::deque<int>> stack;
+
+    int N;
+    std::cin >> N;
+    getchar();
+
+    std::string line;
+    for (int i = 0; i < N; i++) {
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        std::string command;
+        iss >> command;
+        int element;
+        if (command == "push") {
+            iss >> element;
+            stack.push(element);
+        }
+        if (command == "pop") {
+            try {
+                stack.pop();
+            } catch(const std::runtime_error& e) {
+                // 不做任何处理
+                continue;
+            }
+        }
+        if (command == "top") {
+            try {
+                std::cout << stack.top() << std::endl;
+            } catch(const std::runtime_error& e) {
+                std::cout << "null" << std::endl;
+            }   
+        }
+        if (command == "size") {
+            std::cout << stack.size() << std::endl;
+        }
+        if (command == "empty") {
+            std::cout << (stack.empty() ? "true" : "false") << std::endl;
+        }
+    }
+    return 0;
+}
+class Solution {
+public:
+    string reverseStr(string s, int k) 
+    {
+        int n = s.size();
+
+        for (int i = 0; i < n; i += 2 * k)//每次划分出一个区间
+        {
+            // 若剩余字符小于 k 全部反转
+            // 否则反转前 k 个
+            int end = min(i + k - 1, n - 1);
+            reverse(s, i, end);
+        }
+        return s;
+    }
+    void reverse (string &s, int left, int right)
+    {
+        while (left < right)
+        {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+};
+
--- test-25-9-13.cpp
+++ test-25-9-13.cpp
@@ -5,30 +5,55 @@ using namespace std;
 
 class Solution {
 public:
-    bool isHappy(int n) {
-        int cur = n;
-        unordered_set<int> set;
-        while (n != 1)
+    string reverseStr(string s, int k) 
+    {
+        int n = s.size();
+        int count = 0;
+        int two = n / (2 * k);
+
+        for (int i = 0; i < n; i++)
         {
-            int tmp = 0;
-            while (cur)
+            count++;
+            if (count == 2*k)
             {
-                tmp += (cur % 10) * (cur % 10);
-                cur /= 10;
+                // 翻转前 k 个而非后 k 个字符
+                reverse(s, i - 1 - k, i - k);
+                count = 0;
+                --two;//二倍区间减一
+                continue;
             }
-            n = tmp;
-            cur = n;
-            set.insert(n);
-            if (set.find(n) != set.end()) return false;
+            // 之所以出现错误是因为2倍数没有算完
+            if (two == 0 && n - i - 1 < k)
+            {
+                reverse(s, i, n - 1);
+                return s;
+            }
+
+            if (two == 0 && n - i - 1 < 2*k && n - i - 1 >= k)
+            {
+                reverse(s, i, i + k - 1);
+                return s;
+            }
         }
-        return true;
+        return s;
+    }
+    void reverse (string &s, int left, int right)
+    {
+        while (left < right)
+        {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
     }
 };
 
 int main()
 {
     Solution s;
-    bool istrue = s.isHappy(19);
-
+    string result = s.reverseStr("abcdefg", 2);
+    cout << result << endl;
     return 0;
 }
